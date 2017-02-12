81f7e72704e8e82dd9336013bf202fc73ef56d80
Kirill Averyanov <Kirillzzy@gmail.com>
2017-02-12 15:04:07 +0300

    routing

--- SocialMilkServer/Sources/App/main.swift
+++ SocialMilkServer/Sources/App/main.swift
@@ -8,6 +8,50 @@ drop.get { req in
     ])
 }
 
+drop.get("get", "posts", "all"){ request in
+    return "Got allPosts"
+}
+
+drop.get("get", "posts", "vk"){ request in
+    return "Got vkPosts"
+}
+
+drop.get("get", "posts", "twitter"){ request in
+    return "Got twitterPosts"
+}
+
+drop.get("get", "posts", "facebook"){ request in
+    return "Got facebookPosts"
+}
+
+drop.get("get", "checkedposts", "vk"){ request in
+    return "Got checkedPosts vk"
+}
+
+drop.get("get", "checkedposts", "twitter"){ request in
+    return "Got checkedPosts twitter"
+}
+
+drop.get("get", "checkedposts", "facebook"){ request in
+    return "Got checkedPosts facebook"
+}
+
+drop.get("get", "sn"){ request in
+    return "Got list of social networks"
+}
+
+drop.post("auth", "vk"){ request in
+    return "authorized vk"
+}
+
+drop.post("auth", "twitter"){ request in
+    return "authorized twitter"
+}
+
+drop.post("auth", "facebook"){ request in
+    return "authorized facebook"
+}
+
 drop.resource("posts", PostController())
 
 drop.run()
